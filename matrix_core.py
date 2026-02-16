0d78a406db329bacda265901efb38a9c5681f521
BLACKV0LT <samsng1073@gmail.com>
2026-02-16 13:33:14 +0200

    Implement basic matrix operations in matrix_core.py

---
+++ matrix_core.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def summa(A, B):
+    return (A + B)
+    
+def subt(A, B):
+    return A - B
+
+def multi(A, B):
+    return A * B
+    
+def det(A):
+    return np.linalg.det(A)
+
+
+def transp(A):
+    return A.T
+    
+def deg(A, b):
+    return A ** b
+    
